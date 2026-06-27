69c7cca1945ce2ea2dcbff060012107c75bb5126
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-06-27 21:15:49 +0530

    6. Some more properties of  arithmetic & math

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
 # arithmetic & math
 
-friend = 15
+x = 3.14
+y = 4
+z = 5
 
-# Addition(+)
+# result = round(x)
+# result = abs(y)
+# result = pow(4, 3)
+# result = max(x, y, z)
+result = min(x, y, z)
 
-#friend = friend + 1
-#friend += 1
-
-#Subtraction(-)
-
-#friend = friend - 2
-#friend -= 2
-
-#Multiplication(*)
-
-#friend = friend * 3
-#friend *= 3
-
-#Division(/)
-
-#friend = friend / 4
-#friend /= 4
-
-# Exponential(**)
-
-#friend = friend ** 5
-friend **= 5
-
-#Modulus(%)
-
-remainder = friend % 6
-
-print(remainder)
+print(result)
