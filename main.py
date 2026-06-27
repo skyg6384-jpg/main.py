29cd373e187f0d34b780bcf06a5b6ea5e12e7146
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-06-27 20:43:41 +0530

    5.  Madlibs game  using python

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-# Exercise 2 Shopping cart program
+# Madlib game
+# word game where you create a story
+# by filling in blanks with random words
 
-item = input("What item would you like to buy?: ")
-price = float(input("What is the price?: "))
-quantity = int(input("How many world you like?: "))
-total = price * quantity
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing: '")
+adjective3 = input("Enter an adjective (description): ")
 
-print(f"You bought {quantity} X {item}/s")
-print(f"Your total is ₹{total}")
+print(f"Today I went a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
