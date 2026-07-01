93de459d576c3b66be14835327fddf48ca1df361
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-07-01 21:39:29 +0530

    11. logical operators {and,  not}.

--- main.py
+++ main.py
@@ -4,12 +4,27 @@
 #                     not = inverts the condition (not, False, not True)
 
 
-# or
-
+# and
 temp = 20
-is_raining = True
 
-if temp > 35 or temp < 0 or is_raining:
-    print("The outdoor event is cancelled")
-else:
-    print("The outdoor event is still scheduled")
+is_sunny = False
+
+if temp >= 28 and is_sunny:
+    print("It is HOT outside 🥵")
+    print("It is sunny ☀️")
+elif temp <= 0 and is_sunny:
+    print("It is COLD outside 🥶")
+    print("It is sunny ☀️")
+elif  28 > temp > 0 and is_sunny:
+    print("It is WARM outside 😳")
+    print("It is sunny ☀️")
+# not
+elif temp >= 28 and not is_sunny:
+    print("It is HOT outside 🥵")
+    print("It is CLOUDY ☁️")
+elif temp <= 0 and not is_sunny:
+    print("It is COLD outside 🥶")
+    print("It is CLOUDY ☁️")
+elif  28 > temp > 0 and not is_sunny:
+    print("It is WARM outside 😳")
+    print("It is CLOUDY ☁️")
