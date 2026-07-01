4b76ef58d390aa97f989a5d8353b19f411e5afc1
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-07-01 21:01:42 +0530

    10. Temperature conversion program

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-# Python weight converter
+# Temperature conversion program
 
-weight = float(input("Enter your weight: "))
-unit = input("Kilograms or Pounds? (K or L): ")
+unit = input("Is this temperature in Celsius or Fahrenheit (C/F): ")
+temp = float(input("Enter the temperature: "))
 
-if unit == "K":
-    weight = weight * 2.205
-    unit = "Lbs."
-    print(f"Your weight is: {round(weight, 1)} {unit}")
-elif unit == "L":
-    weight = weight / 2.205
-    unit = "Kgs."
-    print(f"Your weight is: {round(weight, 1)} {unit}")
+if unit == "C":
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"The temperature in Fahrenheit is: {temp}°F")
+elif unit == "F":
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f"The temperature in Celsius is: {temp}°C")
 else:
-    print(f"{unit} was not valid")
+    print(f"{unit} is an invalid unit of measurement")
