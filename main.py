6b12ef8fbdcd0570c2f7682cc6883157df6065b5
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-07-01 21:15:38 +0530

    11. logical operators {or}

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# Temperature conversion program
+# logical operators = evaluate multiple conditions (or, and, not)
+#                     or = at least one condition must be True
+#                     and = both conditions must be True
+#                     not = inverts the condition (not, False, not True)
 
-unit = input("Is this temperature in Celsius or Fahrenheit (C/F): ")
-temp = float(input("Enter the temperature: "))
 
-if unit == "C":
-    temp = round((9 * temp) / 5 + 32, 1)
-    print(f"The temperature in Fahrenheit is: {temp}°F")
-elif unit == "F":
-    temp = round((temp - 32) * 5 / 9, 1)
-    print(f"The temperature in Celsius is: {temp}°C")
+# or
+
+temp = 20
+is_raining = True
+
+if temp > 35 or temp < 0 or is_raining:
+    print("The outdoor event is cancelled")
 else:
-    print(f"{unit} is an invalid unit of measurement")
+    print("The outdoor event is still scheduled")
