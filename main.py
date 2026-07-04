99af44e3ee693acf076389bd0f51cf99fb31d6a2
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-07-04 22:25:09 +0530

    13. String Methods, validate user input exercise

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-# string methods
+# String Methods
 
-print(help(str))    # is for help function.
+# validate user input exercise
+# 1. username is no more than 12 characters
+# 2. username must not contain spaces
+# 3. username must not contain digits
 
-#name = input("Enter your full name: ")
-phone_number = input("Enter your phone number # ")
+username = input("Enter a username: ")
 
-# result = len(name)
-# result = name.find("S")
-#result = name.rfind("S")
-# name = name.capitalize()
-# name = name.upper()
-# name = name.lower()
-# result = name.isdigit()
-# result = name.isalpha()
-# result = phone_number.count("-")
-# phone_number = phone_number.replace("-", "")
-
-print(phone_number)
+if len(username) > 12:
+    print("Your username can't be more than 12 characters")
+elif not username.find(" ") == -1:
+    print("Your username can't contain spaces")
+elif not username.isalpha():
+    print("Your username can't contain numbers")
+else:
+    print(f"Welcome {username}")
