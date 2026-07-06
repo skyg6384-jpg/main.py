0f9ed0c442fb2bf86903525440a6849e6b4b2395
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-07-06 23:57:59 +0530

    16. Topic - While loop, Infinite loop & Exercise 1

--- main.py
+++ main.py
@@ -1,73 +1,30 @@
-# format specifiers = {:flags} format a value based on what
-#                              flags are inserted
-
-price1 = 30000.6789
-price2 = -50000.6789
-price3 = 12000.34
-
-# .(number)f = round to that many decimal places (fixed point)
-
-print(f"Price 1 is ₹{price1:.2f}")
-print(f"Price 2 is ₹{price2:.2f}")
-print(f"Price 3 is ₹{price3:.2f}")
-
-# :(number) = allocate that many spaces
-
-print(f"Price 1 is ₹{price1:10}")
-print(f"Price 2 is ₹{price2:10}")
-print(f"Price 3 is ₹{price3:10}")
-
-# :0(number) = allocate and zero pad that many spaces
-
-print(f"Price 1 is ₹{price1:010}")
-print(f"Price 2 is ₹{price2:010}")
-print(f"Price 3 is ₹{price3:010}")
-
-# :<(number) = left justify
-
-print(f"Price 1 is ₹{price1:<10}")
-print(f"Price 2 is ₹{price2:<10}")
-print(f"Price 3 is ₹{price3:<10}")
-
-# :>(number) = .right justify
-
-print(f"Price 1 is ₹{price1:>10}")
-print(f"Price 2 is ₹{price2:>10}")
-print(f"Price 3 is ₹{price3:>10}")
-
-# :^(number) = center align
-
-print(f"Price 1 is ₹{price1:^10}")
-print(f"Price 2 is ₹{price2:^10}")
-print(f"Price 3 is ₹{price3:^10}")
-
-# :+ = use a plus sign to indicate positive value
-
-print(f"Price 1 is ₹{price1:+}")
-print(f"Price 2 is ₹{price2:+}")
-print(f"Price 3 is ₹{price3:+}")
-
-# := = place sign to leftmost position
-
-print(f"Price 1 is ₹{price1:=}")
-print(f"Price 2 is ₹{price2:=}")
-print(f"Price 3 is ₹{price3:=}")
-
-# :  = insert a space before position numbers
-
-print(f"Price 1 is ₹{price1: }")
-print(f"Price 2 is ₹{price2: }")
-print(f"Price 3 is ₹{price3: }")
-
-# :, = comma separator
-
-print(f"Price 1 is ₹{price1:,}")
-print(f"Price 2 is ₹{price2:,}")
-print(f"Price 3 is ₹{price3:,}")
+# While loop = execute some code WHILE some condition remains true
 
 
-# Exercise Mix Method
+# While loop
 
-print(f"Price 1 is ₹{price1:+,.2f}")
-print(f"Price 2 is ₹{price2:+,.2f}")
-print(f"Price 3 is ₹{price3:+,.2f}")
+name = input("Enter your name: ")
+
+while name == "":
+    print("You did not enter your name")
+    name = input("Enter your name: ")
+print(f"Hello {name}")
+
+# Infinite loop ♾️
+
+ok = input("Enter you are ok: ")
+
+while ok == "":
+    print("You are ok")
+
+print(f"Hello {ok}")
+
+# Exercise 1
+
+age = int(input("Enter your age: "))
+
+while age < 0:
+    print("Age can't be negative")
+    age = int(input("Enter your age: "))
+
+print(f"You are {age} years old")
