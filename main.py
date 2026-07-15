f4837a84926799d23a13632467503559f3663445
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-07-15 20:42:10 +0530

    19. Countdown Timer Program

--- main.py
+++ main.py
@@ -1,38 +1,15 @@
-# for loops = execute a block of code a fixed number of times.
-#             You can iterate over a range, string, sequence, etc.
+# python countdown timer program
 
-for x in range(1,11):   # range function
-    print(x)
+import time
+from idlelib.debugobj import myrepr
 
-print("HAPPY NEW YEAR!")
-print("HAPPY NEW YEAR!")
+my_time = int(input("Enter the time in seconds: "))
 
-for y in reversed(range(1, 11)):  # reversed range function
-    print(y)
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
 
-for z in range(1, 11, 2):  # 2 is steps
-    print(z)
-
-
-# Example using credit card number
-
-credit_card = "1234-5678-9012-3456"
-
-for card_num in credit_card:
-    print(card_num)
-
-# Example, Print numbers from 1 to 20. When the number is 13, continue skips it.
-
-for n in range(1, 21):
-    if n == 13:
-        continue
-    else:
-        print(n)
-
-# Example, numbers from 1 to 20. When the number is 13, break it.
-
-for m in range(1, 21):
-    if m == 13:
-        break
-    else:
-        print(m)
+print("TIME'S UP!")
