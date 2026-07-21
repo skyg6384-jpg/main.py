730371426dd321117ba57bf371cdec96ee0b2080
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-07-21 23:07:04 +0530

    24. 2D Collections, Exercise 2D Keypad

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
-# Shopping cart program
+# 2D Collections
 
-foods = []
-prices = []
-total = 0
+fruits =     ["apple", "banana", "coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats =      ["chicken", "fish", "turkey"]
 
-while True:
-    food = input("Enter a food to buy (q to quit): ")
-    if food.lower() == "q":
-        break
-    else:
-        price = float(input(f"Enter the price of a {food}: $"))
-        foods.append(food)
-        prices.append(price)
+groceries = [fruits, vegetables, meats]
 
-print("----- YOUR CART -----")
-
-for food in foods:
-    print(food, end=" ")
-
-for price in prices:
-    total += price
+print(groceries[0][0])
+print(groceries[1][0])
+print(groceries[2][0])
 
 print()
-print(f"Your total is: ${total}")
+for collection in groceries:
+    for food in collection:
+        print(food, end=" ")
+    print()
+
+print()
+names = ({"sky", "apple", "banana", "coconut"},
+         {"www", "jon", "space", "earth"},
+         {"god", "sun", "code"})
+
+
+for collection in names:
+    for name in collection:
+        print(name, end=" ")
+    print()
+
+print()
+
+# Exercise 2D Keypad
+
+num_pad = ((1, 2, 3),
+           (4, 5, 6),
+           (7, 8, 9),
+           ("*", 0, "#"))
+
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
+    print()
