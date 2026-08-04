97f231f5f9291a56c5ff17b86937f5bd75d4e2ad
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-08-04 22:05:45 +0530

    32. default arguments & Exercise TIMER

--- main.py
+++ main.py
@@ -1,62 +1,26 @@
-# function = A block of reusable code
-#            place () after the function name to invoke it
+# default arguments = A default value for certain parameters
+#                     default is used when that argument is omitted
+#                     make your functions more flexible, reduces # of arguments
+#                     1. positional, 2. DEFAULT, 3. Keyword, 4. arbitrary
 
-def happy_birthday(name, age):
-    print(f"Happy Birthday to {name}!")
-    print(f"You are {age} year old!")
-    print("Happy Birthday to you!")
-    print()
+def net_price(list_price, discount=0, tax=0.05):
+    return list_price * (1 - discount) * (1 + tax)
 
-happy_birthday("Sky God", 21)
-happy_birthday("Tyson", 42)
-happy_birthday("Tom", 28)
-
-
-# Example
-
-def display_invoice(username, amount, due_date):
-    print(f"Hello {username}")
-    print(f"Your bill of ₹{amount:.2f} is due: {due_date}")
-
-display_invoice("Sky God", 6000, "01/08/2026")
-display_invoice("Tyson", 500000, "05/08/2026")
-display_invoice("tom", 100500.32, "05/09/2028")
-
-# -----------------
-# -----------------
-
-# return = statement used to end a function
-#          and send a result back to the caller
-
-def add(x, y):
-    z = x + y
-    return z
-
-def subtract(x, y):
-    z = x - y
-    return z
-
-def multiply(x, y):
-    z = x * y
-    return z
-
-def divide(x, y):
-    z = x / y
-    return z
-
-print(add(1, 2))
-print(subtract(1, 2))
-print(multiply(1, 2))
-print(divide(1, 2))
+print(net_price(60000))
+print(net_price(250, 0.1))
+print(net_price(500, 0.1, 0))
 
 
-# Example
+# ------------
+# Exercise TIMER
+# ------------
 
-def create_name(first, last):
-    first = first.capitalize()
-    last = last.capitalize()
-    return first + " " + last
+import time
 
-full_name = create_name("Sky", "God")
+def count(end, start=0):
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+    print("DONE!")
 
-print(full_name)
+count(10)
