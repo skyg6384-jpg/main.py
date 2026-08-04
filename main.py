8a14ee1ab3128cc3f1b4e2ce24be4dedd9bdb6eb
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-08-04 20:48:51 +0530

    31. function & return

--- main.py
+++ main.py
@@ -1,59 +1,62 @@
-# Dice Roller Program
+# function = A block of reusable code
+#            place () after the function name to invoke it
 
-import random
-
-# print("\u25CF \u250C \u2500 \u2510 \u2502 \u2514 \u2518")
-# ● ┌ ─ ┐ │ └ ┘
-
-"┌─────────┐"
-"│         │"
-"│         │"
-"│         │"
-"└─────────┘"
-
-dice_art = {1: ("┌─────────┐",
-                "│         │",
-                "│    ●    │",
-                "│         │",
-                "└─────────┘"), 2: ("┌─────────┐",
-                                    "│ ●       │",
-                                    "│         │",
-                                    "│       ● │",
-                                    "└─────────┘"), 3: ("┌─────────┐",
-                                                        "│ ●       │",
-                                                        "│    ●    │",
-                                                        "│       ● │",
-                                                        "└─────────┘"), 4: ("┌─────────┐",
-                                                                            "│ ●     ● │",
-                                                                            "│         │",
-                                                                            "│ ●     ● │",
-                                                                            "└─────────┘"), 5: ("┌─────────┐",
-                                                                                                "│ ●     ● │",
-                                                                                                "│    ●    │",
-                                                                                                "│ ●     ● │",
-                                                                                                "└─────────┘"),
-            6: ("┌─────────┐",
-                "│ ●     ● │",
-                "│ ●     ● │",
-                "│ ●     ● │",
-                "└─────────┘")}
-
-dice = []
-total = 0
-num_of_dice = int(input("How many dice?: "))
-
-for die in range(num_of_dice):
-  dice.append(random.randint(1, 6))
-
-# for die in range(num_of_dice):
-#     for line in dice_art.get(dice[die]):
-#          print(line)
-
-for line in range(5):
-    for die in dice:
-        print(dice_art.get(die)[line], end="")
+def happy_birthday(name, age):
+    print(f"Happy Birthday to {name}!")
+    print(f"You are {age} year old!")
+    print("Happy Birthday to you!")
     print()
 
-for die in dice:
-    total += die
-print(f"total: {total}")
+happy_birthday("Sky God", 21)
+happy_birthday("Tyson", 42)
+happy_birthday("Tom", 28)
+
+
+# Example
+
+def display_invoice(username, amount, due_date):
+    print(f"Hello {username}")
+    print(f"Your bill of ₹{amount:.2f} is due: {due_date}")
+
+display_invoice("Sky God", 6000, "01/08/2026")
+display_invoice("Tyson", 500000, "05/08/2026")
+display_invoice("tom", 100500.32, "05/09/2028")
+
+# -----------------
+# -----------------
+
+# return = statement used to end a function
+#          and send a result back to the caller
+
+def add(x, y):
+    z = x + y
+    return z
+
+def subtract(x, y):
+    z = x - y
+    return z
+
+def multiply(x, y):
+    z = x * y
+    return z
+
+def divide(x, y):
+    z = x / y
+    return z
+
+print(add(1, 2))
+print(subtract(1, 2))
+print(multiply(1, 2))
+print(divide(1, 2))
+
+
+# Example
+
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("Sky", "God")
+
+print(full_name)
