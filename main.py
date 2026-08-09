686daff08107d4cb858a20ce77774a68b7677dfe
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-08-09 22:18:24 +0530

    36. Membership operators

--- main.py
+++ main.py
@@ -1,34 +1,73 @@
-# Iterables = An object/collection that can return its elements one at a time,
-#             allowing it to be iterated over in a loop
-
-numbers = [1, 2, 3, 4, 5]   # class 'int'
-
-for number in reversed(numbers):
-    print(number)
-print()
+# Membership operators = used to test whether a value or variable is found in a sequence
+#                        (string, list, tuple, set, or dictionary)
+#                        1. in
+#                        2. not in
 
 
-items = ("apple", "banana", "cherry")   # class 'str'
+# Example 1
+word = "Apple"
 
-for item in reversed(items):
-    print(item, end=" ")
-print()
-print()
+letter = input("Guess a letter in the secret word: ")
+
+if letter in word:
+    print(f"There is a {letter}")
+else:
+    print(f"{letter} was not found")
+
+# or
+
+words = "Banana"
+
+letters = input("Guess a letter in the secret word: ")
+
+if letters not in word:
+    print(f"{letters} was not found")
+else:
+    print(f"There is a {letters}")
 
 
-# ---------
-# 'set' object is not reversible
-# ---------
+# Example 2
 
-name = "Sky God"
+students = {"SkyGod", "Spongebob", "Sanday"}
 
-for name in reversed(name):
-    print(name, end="")
-print()
-print()
+student = input("Enter the name of a student: ")
+
+if student in students:
+    print(f"{student} is a student")
+else:
+    print(f"{student} was not found")
 
 
-my_dictionary = {"A": 1, "B": 2, "C": 3}
+# or
 
-for key, value in my_dictionary.items():
-    print(f"{key} = {value}")
+students2 = {"SkyGod", "Spongebob", "Sanday"}
+
+student2 = input("Enter the name of a student: ")
+
+if student2 not in students2:
+    print(f"{student2} was not found")
+    print(f"{student2} is a student")
+else:
+    print(f"{student2} is a student")
+
+# Example 3 in dictionary
+grades = {"Sandy": "A",
+          "SkyGod": "B+",
+          "Spongebob": "C",
+          "Sanday": "D"}
+
+student3 = input("Enter the name of a student: ")
+
+if student3 in grades:
+    print(f"{student3}'s grade is {grades[student3]}")
+else:
+    print(f"{student3} was not found")
+
+# Example 4
+
+email = "SkyGod@gmail.com"
+
+if "@" in email and "." in email:
+    print(f"Valid email")
+else:
+    print(f"Invalid email")
