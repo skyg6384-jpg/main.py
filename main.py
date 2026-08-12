1a64c61e5b556a21d9872ad1a2e7f62e45bdc84e
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-08-12 22:47:10 +0530

    39. module, main.py & example.py

--- main.py
+++ main.py
@@ -1,50 +1,40 @@
-# Match-case statement (switch): An alternative to using many 'elif' statements
-#                                Execute some code if a value matches a 'case'
-#                                Benefits: cleaner and syntax is more readable
+# ---------- main.py ----------
+# module = a file containing code you want to include in your program
+#          use 'import' to include a module (built-in or your own)
+#          useful to break up a large program reusable separate files
 
 
+#print(help("modules"))
+#print(help("math"))
 
-#Example 1
+# Example
+
+#import math
+#print(math.pi)
+
+#import math as m
+#print(m.pi)
+
+#from math import e
+#print(e)
+
+import math
+
+a, b, c, d, e = 1, 2, 3, 4, 5
+
+print(math.e ** a)
+print(math.e ** b)
+print(math.e ** c)
+print(math.e ** d)
+print(math.e ** e)
 
 
-def day_of_week(day):
-    match day:
-        case 1:
-            return "It is Sunday"
-        case 2:
-            return "It is Monday"
-        case 3:
-            return "It is Tuesday"
-        case 4:
-            return "It is Wednesday"
-        case 5:
-            return "It is Thursday"
-        case 6:
-            return "It is Friday"
-        case 7:
-            return "It is Saturday"
-        case _:
-            return "Not a valid day"
+import example
 
-print(day_of_week(1))
+result = example.pi
+result = example.square(3)
+result = example.cube(3)
+result = example.circumference(3)
+result = example.area(3)
 
-
-# Example 2
-
-def is_weekend(day):
-    match day:
-        case "Sunday" | "Saturday":
-            return True
-        case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
-            return False
-        case _:
-            return False
-
-print(is_weekend("Sunday"))
-print(is_weekend("Monday"))
-print(is_weekend("Tuesday"))
-print(is_weekend("Wednesday"))
-print(is_weekend("Thursday"))
-print(is_weekend("Friday"))
-print(is_weekend("Saturday"))
-print(is_weekend("Pizza"))
+print(result)
