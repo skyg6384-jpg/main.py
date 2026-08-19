04459a8c9f117ae62aafaa1f347b83ce9c6c6100
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-08-19 23:57:01 +0530

    42. Python Banking Program

--- main.py
+++ main.py
@@ -1,54 +1,72 @@
-# Scope Resolution
+# Python Banking Program
 
-# variable scope = where a variable and accessible
-# scope resolution = (LEGB) Local -> Enclosed -> Global -> Built-in
+def show_balance(balance):
+    print("*******************")
+    print(f"Your balance is ₹{balance:.2f}")
+    print("*******************")
 
-# ------------
-# Local
+def deposit():
+    print("*******************")
+    amount = float(input("Enter an amount to deposit: "))
+    print("*******************")
 
-def func1():
-    x = 1
-    print(x)
+    if amount < 0:
+        print("*******************")
+        print("That's not a valid amount")
+        print("*******************")
+        return 0
+    else:
+        return amount
 
-def func2():
-    x = 2
-    print(x)
+def withdraw(balance):
+    print("*******************")
+    amount = float(input("Enter amount to be withdrawn: "))
+    print("*******************")
 
-func1()
-func2()
+    if amount > balance:
+        print("*******************")
+        print("Insufficient funds")
+        print("*******************")
+        return 0
+    elif amount < 0:
+        print("*******************")
+        print("Amount must be greater than 0")
+        print("*******************")
+        return 0
+    else:
+        return amount
 
-# ------------
-# Enclosed
+def main():
+    balance = 0
+    is_running = True
 
-def func3():
-    y = 3
+    while is_running:
+        print("*******************")
+        print("  Banking Program  ")
+        print("*******************")
+        print("1. Show Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
+        print("*******************")
+        choice = input("Enter your choice (1-4): ")
 
-    def func4():
-        print(y)
-    func4()
+        if choice == '1':
+            show_balance(balance)
+        elif choice == '2':
+            balance += deposit()
+        elif choice == '3':
+            balance -= withdraw(balance)
+        elif choice == '4':
+            is_running = False
+        else:
+            print("*******************")
+            print("That is not a valid choice")
+            print("*******************")
 
-func3()
+        print("*******************")
+        print("Thank you! Have a nice day!")
+        print("*******************")
 
-# ------------
-# Global
-
-def func5():
-    print(z)
-
-def func6():
-    print(z)
-
-z = 5
-
-func5()
-func6()
-
-# ------------
-# Built-in
-
-from math import e
-
-def func7():
-    print(e)
-
-func7()
+if __name__ == '__main__':
+    main()
