292796c68a5d24106374da476cf9500e697a01d9
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-08-23 23:52:30 +0530

    44. Encryption Program

--- main.py
+++ main.py
@@ -1,82 +1,38 @@
-# Python Slot Machine
+# Encryption Program
 
 import random
+import string
 
-def spin_row():
-    symbols = ['🍒', '🍉', '🍋', '🔔', '⭐']
+#chars = string.punctuation + string.digits
+#chars = string.punctuation + string.digits + string.ascii_letters
+#chars = string.whitespace + string.punctuation + string.digits + string.ascii_letters
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
 
-    return [random.choice(symbols) for _ in range(3)]
+random.shuffle(key)
 
+# print(f"chars: {chars}")
+# print(f"key  : {key}")
 
-def print_row(row):
-    print("*************")
-    print(" | ".join(row))
-    print("*************")
+#ENCRYPT
+plain_text = input("Enter a message to encrypt: ")
+cipher_text = ""
 
-def get_payout(row, bet):
-    if row[0] == row[1] == row[2]:
-        if row[0] == '🍒':
-            return bet * 3
-        elif row[0] == '🍉':
-            return bet * 4
-        elif row[0] == '🍋':
-            return bet * 5
-        elif row[0] == '🔔':
-            return bet * 10
-        elif row[0] == '⭐':
-            return bet * 20
-    return 0
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
 
-def main():
-    balance = 100
+print(f"original message: {plain_text}")
+print(f"encrypted text: {cipher_text}")
 
-    print("************************")
-    print("Welcome to Python Slots ")
-    print("Symbols: 🍒 🍉 🍋 🔔 ⭐")
-    print("************************")
+#DECRYPT
+cipher_text = input("Enter a message to encrypt: ")
+plain_text = ""
 
-    while balance > 0:
-        print(f"Current balance: ₹{balance}")
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
 
-        bet = input("Place your bet amount: ")
-
-        if not bet.isdigit():
-            print("Please enter a valid number")
-            continue
-
-        bet = int(bet)
-
-        if bet > balance:
-            print("Insufficient funds")
-            continue
-
-        if bet <= 0:
-            print("Bet must be greater than 0")
-            continue
-
-        balance -= bet
-
-        row = spin_row()
-        print("Spinning...\n")
-        print_row(row)
-
-        payout = get_payout(row, bet)
-
-        if payout > 0:
-            print(f"You won ₹{payout}")
-        else:
-            print("Sorry you lost this round")
-
-        balance += payout
-
-        play_again = input("Do you want to spin again? (Y/N): ").upper()
-
-        if play_again != "Y":
-            break
-
-    print("********************************************")
-    print(f"Game over! Your final balance is ₹{balance}")
-    print("********************************************")
-
-if __name__ == '__main__':
-    main()
+print(f"original message: {cipher_text}")
+print(f"encrypted text: {plain_text}")
