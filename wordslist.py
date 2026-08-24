6a016b1974310e2bbbde5ee486ecd87c33e4496f
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-08-25 00:14:46 +0530

    45. Hangman in python

---
+++ wordslist.py
@@ -0,0 +1,33 @@
+# words for hangman game
+
+words = [
+    "aardvark", "alligator", "alpaca", "ant", "anteater", "antelope", "ape",
+    "armadillo", "baboon", "badger", "bat", "bear", "beaver", "bee", "bison",
+    "boar", "buffalo", "butterfly", "camel", "capybara", "caribou", "cat",
+    "caterpillar", "cattle", "chamois", "cheetah", "chicken", "chimpanzee",
+    "chinchilla", "chough", "clam", "cobra", "cockroach", "cod", "coyote",
+    "crab", "crane", "crocodile", "crow", "curlew", "deer", "dinosaur", "dog",
+    "dogfish", "dolphin", "donkey", "dormouse", "dotterel", "dove", "dragonfly",
+    "duck", "dugong", "dunlin", "eagle", "echidna", "eel", "eland", "elephant",
+    "elephantseal", "elk", "emu", "falcon", "ferret", "finch", "fish", "flamingo",
+    "fly", "fox", "frog", "gaur", "gazelle", "gerbil", "giantpanda", "giraffe",
+    "gnat", "gnu", "goat", "goldfinch", "goldfish", "goose", "gorilla", "goshawk",
+    "grasshopper", "grouse", "guanaco", "gull", "hamster", "hare", "hawk", "hedgehog",
+    "heron", "herring", "hippopotamus", "hornet", "horse", "human", "hummingbird",
+    "hyena", "ibex", "ibis", "jackal", "jaguar", "jay", "jellyfish", "kangaroo",
+    "kingfisher", "koala", "komododragon", "kookabura", "kouprey", "kudu", "lapwing",
+    "lark", "lemur", "leopard", "lion", "llama", "lobster", "locust", "loris", "louse",
+    "lyrebird", "magpie", "mallard", "manatee", "mandrill", "mantis", "marten", "meerkat",
+    "mink", "mole", "mongoose", "monkey", "moose", "mosquito", "mouse", "mule", "narwhal",
+    "newt", "nightingale", "octopus", "okapi", "opossum", "oryx", "ostrich", "otter",
+    "owl", "ox", "oyster", "panther", "parrot", "partridge", "peafowl", "pelican",
+    "penguin", "pheasant", "pig", "pigeon", "polarbear", "pony", "porcupine",
+    "porpoise", "quail", "quelea", "quetzal", "rabbit", "raccoon", "rail", "ram",
+    "rat", "raven", "reddeer", "redpanda", "reindeer", "rhinoceros", "rook",
+    "salamander", "salmon", "sanddollar", "sandpiper", "sardine", "scorpion",
+    "seahorse", "seal", "shark", "sheep", "shrew", "skunk", "snail", "snake",
+    "sparrow", "spider", "spoonbill", "squid", "squirrel", "starling", "stingray",
+    "stinkbug", "stork", "swallow", "swan", "tapir", "tarsier", "termite",
+    "tiger", "toad", "trout", "turkey", "turtle", "viper", "vulture", "wallaby",
+    "walrus", "wasp", "weasel", "whale", "wolf", "wolverine", "wombat", "wren", "yak", "zebra"
+]
