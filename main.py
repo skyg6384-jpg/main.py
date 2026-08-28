639e0b27045b79ce48af3eb8124c3bc9802afc3a
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-08-28 21:07:11 +0530

    47. Class Variables

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-# object = A "bundle" of related attributes (variables) and methods (function)
-#          Ex. phone, cup, book
-#          You need a "class" to create many objects
+# Class Variable
+# class variable = Shared among all instances of a class
+# Defined outside the constructor
+# Allow you to share data among all object created from that class
 
-# class = (blueprint) user to design the structure and layout of an object
+class Student:
 
-# -------------
+    class_year = 2026
+    num_Student = 0
 
-from car import Car
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Student.num_Student += 1
 
+Student1 = Student("Spongebob", 30)
+Student2 = Student("Patrick", 35)
+Student3 = Student("DAV", 51)
+Student4 = Student("SkyGod", 21)
 
-car1 = Car("Mercedes", 1999, "blue", False)
-car2 = Car("Bugatti Chiron", 2025, "black", True)
+print(f"My graduating class year of {Student1.class_year} has {Student1.num_Student} students")
 
-print(car1.model)
-print(car1.year)
-print(car1.color)
-print(car1.for_sale)
-car1.drive()  #drive
-car1.stop()  #stop
-car1.describe()  #describe
-print()
-
-print(car2.model)
-print(car2.year)
-print(car2.color)
-print(car2.for_sale)
-car2.drive()  #drive
-car2.stop()  #stop
-car2.describe()  #describe
+print(f"Name: {Student1.name}, age: {Student1.age}")
+print(f"Name: {Student2.name}, age: {Student2.age}")
+print(f"Name: {Student3.name}, age: {Student3.age}")
+print(f"Name: {Student4.name}, age: {Student4.age}")
