7c3c3f07522786577f588d35bb965c443611bf9e
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-09-07 23:30:50 +0530

    52. Duck typing

--- main.py
+++ main.py
@@ -1,47 +1,27 @@
-# Polymorphism = Greek word that means to "have many forms or faces"
-#                Poly = Many
-#                Morphe = Form
+# "Duck typing" = Another way to achieve polymorphism besides Inheritance
+#                 Object must have the minimum necessary attributes/methods
+#                 "If it looks like a duck and quacks like a duck, it must be a duck."
 
-#                TWO WAYS TO ACHIEVE POLYMORPHISM
-#                1. Inheritance = An object could be treated of the same type as a parent class
-#                2. "Duck typing" Object must have necessary attributes/methods
+class Animal:
+    alive = True
 
-from abc import ABC, abstractmethod
+class Dog(Animal):
+    def speak(self):
+        print("WOOF!")
 
-class Shape(ABC):
+class Cat(Animal):
+    def speak(self):
+        print("MEOW!")
 
-    @abstractmethod
-    def area(self):
-        pass
+class Car:
 
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
+    alive = False
 
-    def area(self):
-        return 3.14 * self.radius ** 2
+    def speak(self):
+        print("HONK!")
 
-class Square(Shape):
-    def __init__(self, side):
-        self.side = side
+animal = [Dog(), Cat(), Car()]
 
-    def area(self):
-        return self.side **2
-
-class Triangle(Shape):
-    def __init__(self, base, height):
-        self.base = base
-        self.height = height
-
-    def area(self):
-        return self.base * self.height * 0.5
-
-class Pizza(Circle):
-    def __init__(self, topping, radius):
-        super().__init__(radius)
-        self.topping = topping
-
-shapes = [Circle(4), Square(5), Triangle(6, 7), Pizza("pepperoni", 15)]
-
-for shape in shapes:
-    print(f"{shape.area()}cm^2")
+for animal in animal:
+    animal.speak()
+    print(animal.alive)
