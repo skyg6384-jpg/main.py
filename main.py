b90e64ca59f40797a953b2ac70747ba261476519
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-09-11 22:07:27 +0530

    56. @property

--- main.py
+++ main.py
@@ -1,51 +1,51 @@
-# Magic methods = Dunder methods (double underscore) __init__, __str__, __eq__
-#                 They are automatically called by many of Python's built-in operations.
-#                 They allow developers to define or customize the behavior of objects
+# @property = Decorator used to define a method as a property (it can be accessed like an attribute)
+#             Benefit: Add additional logic when read, write, or delete attributes
+#             Gives you getter, setter, and deleter method
 
-class Book:
+class Rectangle:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
 
-    def __init__(self, title, author, num_pages):
-        self.title = title
-        self.author = author
-        self.num_pages = num_pages
+    @property
+    def width(self):
+        return f"{self._width:.1f}cm"
 
-    def __str__(self):
-        return f"'{self.title}' by {self.author}"
+    @property
+    def height(self):
+        return f"{self._height:.1f}cm"
 
-    def __eq__(self, other):
-        return self.title == other.title and self.author == other.author
-
-    def __lt__(self, other):
-        return self.num_pages < other.num_pages
-
-    def __gt__(self, other):
-        return self.num_pages > other.num_pages
-
-    def __add__(self,other):
-        return f"{self.num_pages + other.num_pages} pages"
-
-    def __contains__(self, keyword):
-        return keyword in self.title or keyword in self.author
-
-    def __getitem__(self, key):
-        if key == "title":
-            return self.title
-        if key == "author":
-            return self.author
+    @width.setter
+    def width(self, new_width):
+        if new_width > 0:
+            self._width = new_width
         else:
-            return f"key '{key}' was not found"
+            print("Width must be greater than zero")
 
-book1 = Book("ASTROPHYSICS for PEOPLE in a HURRY", "Neil DeGrasse Tyson", 211)
-book2 = Book("In Search of Schrödinger's Cat", "John Gribbin", 252)
-book3 = Book("Chess Book for Beginners", "A.Gopalaratnam", 385)
-book4 = Book("In Search of Schrödinger's Cat", "John Gribbin", 272)
+    @height.setter
+    def height(self, new_height):
+        if new_height > 0:
+            self._height = new_height
+        else:
+            print("Height must be greater than zero")
 
-print(book1)  # __str__
-print(book2 == book4)  # __eq__
-print(book2 > book4)  # __lt__
-print(book2 < book4)  # __gt__
-print(book2 + book4)  # __add__
-print("Cat" in book2)  # __contains__
-print(book1['title'])  # __getitem__
-print(book1['author'])  # __getitem__
-print(book1['audio'])  # __getitem__
+    @width.deleter
+    def width(self):
+        del self._width
+        print("Width has been deleted")
+
+    @height.deleter
+    def height(self):
+        del self._height
+        print("Height has been deleted")
+
+rectangle = Rectangle(3, 4)
+
+rectangle.width = 10
+rectangle.height = 5
+
+#del rectangle.width
+#del rectangle.height
+
+print(rectangle.width)
+print(rectangle.height)
