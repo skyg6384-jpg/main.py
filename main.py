4334ee331e5aa0c5b54815ae6677f40f84bdbe83
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-09-12 22:35:34 +0530

    57. Decorator

--- main.py
+++ main.py
@@ -1,51 +1,25 @@
-# @property = Decorator used to define a method as a property (it can be accessed like an attribute)
-#             Benefit: Add additional logic when read, write, or delete attributes
-#             Gives you getter, setter, and deleter method
+# Decorator = A function that extends the behavior of another function
+#             w/o modifying the base function
+#             Pass the base function as an argument to the decorator
 
-class Rectangle:
-    def __init__(self, width, height):
-        self._width = width
-        self._height = height
+#             @add_sprinkles
+#             get_ice_cream("vanilla")
 
-    @property
-    def width(self):
-        return f"{self._width:.1f}cm"
+def add_sprinkles(func):
+    def wrapper(*args, **kwargs):
+        print("*You add sprinkles 🎊*")
+        func(*args, **kwargs)
+    return wrapper
 
-    @property
-    def height(self):
-        return f"{self._height:.1f}cm"
+def add_fudge(func):
+    def wrapper(*args, **kwargs):
+        print("*You add fudge 🍫*")
+        func(*args, **kwargs)
+    return wrapper
 
-    @width.setter
-    def width(self, new_width):
-        if new_width > 0:
-            self._width = new_width
-        else:
-            print("Width must be greater than zero")
+@add_sprinkles
+@add_fudge
+def get_ice_cream(flavor):
+    print(f"Here is your {flavor} ice cream 🍨")
 
-    @height.setter
-    def height(self, new_height):
-        if new_height > 0:
-            self._height = new_height
-        else:
-            print("Height must be greater than zero")
-
-    @width.deleter
-    def width(self):
-        del self._width
-        print("Width has been deleted")
-
-    @height.deleter
-    def height(self):
-        del self._height
-        print("Height has been deleted")
-
-rectangle = Rectangle(3, 4)
-
-rectangle.width = 10
-rectangle.height = 5
-
-#del rectangle.width
-#del rectangle.height
-
-print(rectangle.width)
-print(rectangle.height)
+get_ice_cream("chocolate")
