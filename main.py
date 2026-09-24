a860c43c572c988b8fb6e622df544a102ef74da9
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-09-24 23:36:19 +0530

    67. PyQt5 labels

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-# PyQt5 introduction
+# PyQt5 labels
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My first GUI")
         self.setGeometry(700, 300, 500, 500)  # self.setGeometry(x, y, width, height)
-        self.setWindowIcon(QIcon("The Fool 1.jpg"))
+
+        label = QLabel("Hello", self)
+        label.setFont(QFont("Arial", 40))
+        label.setGeometry(0, 0, 500, 100)
+        label.setStyleSheet("color: #292929;"
+                            "background-color: #6fdcf7;"
+                            "font-weight: bold;"
+                            "font-style: italic;"
+                            "text-decoration: underline;")
+
+        #label.setAlignment(Qt.AlignTop) #VERTICALLY TOP
+        #label.setAlignment(Qt.AlignBottom)  # VERTICALLY BOTTOM
+        #label.setAlignment(Qt.AlignVCenter)  # VERTICALLY CENTER
+
+        #label.setAlignment(Qt.AlignRight)  # HORIZONTALLY RIGHT
+        #label.setAlignment(Qt.AlignHCenter)  # HORIZONTALLY CENTER
+        #label.setAlignment(Qt.AlignLeft)  # HORIZONTALLY LEFT
+
+        #label.setAlignment(Qt.AlignHCenter | Qt.AlignTop)  # CENTER & TOP
+        #label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)  # CENTER & BOTTOM
+        #label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)  # CENTER & CENTER
+        label.setAlignment(Qt.AlignCenter)  # CENTER & CENTER
+
 
 def main():
     app = QApplication(sys.argv)
