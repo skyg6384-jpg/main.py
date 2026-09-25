4371a54473c2fbe5588fbaf6af90c649e1a79182
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-09-25 18:41:32 +0530

    68. PyQt5 images

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
-# PyQt5 labels
+# PyQt5 images
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QFont
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(700, 300, 500, 500)  # self.setGeometry(x, y, width, height)
 
-        label = QLabel("Hello", self)
-        label.setFont(QFont("Arial", 40))
-        label.setGeometry(0, 0, 500, 100)
-        label.setStyleSheet("color: #292929;"
-                            "background-color: #6fdcf7;"
-                            "font-weight: bold;"
-                            "font-style: italic;"
-                            "text-decoration: underline;")
+        label = QLabel(self)
+        label.setGeometry(0, 0, 250, 250)
 
-        #label.setAlignment(Qt.AlignTop) #VERTICALLY TOP
-        #label.setAlignment(Qt.AlignBottom)  # VERTICALLY BOTTOM
-        #label.setAlignment(Qt.AlignVCenter)  # VERTICALLY CENTER
+        pixmap = QPixmap("The Fool 1.jpg")
+        label.setPixmap(pixmap)
 
-        #label.setAlignment(Qt.AlignRight)  # HORIZONTALLY RIGHT
-        #label.setAlignment(Qt.AlignHCenter)  # HORIZONTALLY CENTER
-        #label.setAlignment(Qt.AlignLeft)  # HORIZONTALLY LEFT
+        label.setScaledContents(True)
 
-        #label.setAlignment(Qt.AlignHCenter | Qt.AlignTop)  # CENTER & TOP
-        #label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)  # CENTER & BOTTOM
-        #label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)  # CENTER & CENTER
-        label.setAlignment(Qt.AlignCenter)  # CENTER & CENTER
+        label.setGeometry((self.width() - label.width()) // 2,
+                          (self.height() - label.height()) // 2,
+                          label.width(),
+                          label.height())
 
 
 def main():
@@ -40,3 +30,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
