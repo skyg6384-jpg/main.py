642021d321096b16b7591b113ab489f253d9d038
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-09-25 21:18:11 +0530

    69. PyQt5 layout managers

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
-# PyQt5 images
+# PyQt5 layouts
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel,
+                             QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(700, 300, 500, 500)  # self.setGeometry(x, y, width, height)
+        self.setGeometry(700, 300, 500, 500)
+        self.initUI()
 
-        label = QLabel(self)
-        label.setGeometry(0, 0, 250, 250)
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
 
-        pixmap = QPixmap("The Fool 1.jpg")
-        label.setPixmap(pixmap)
+        label1 = QLabel("#1", self)
+        label2 = QLabel("#2", self)
+        label3 = QLabel("#3", self)
+        label4 = QLabel("#4", self)
+        label5 = QLabel("#5", self)
 
-        label.setScaledContents(True)
+        label1.setStyleSheet("background-color: red;")
+        label2.setStyleSheet("background-color: yellow;")
+        label3.setStyleSheet("background-color: green;")
+        label4.setStyleSheet("background-color: blue;")
+        label5.setStyleSheet("background-color: purple;")
 
-        label.setGeometry((self.width() - label.width()) // 2,
-                          (self.height() - label.height()) // 2,
-                          label.width(),
-                          label.height())
+        #vbox = QVBoxLayout()
+        #hbox = QHBoxLayout()
+        grid = QGridLayout()
+
+        grid.addWidget(label1, 0, 0)   # vbox, hbox, grid
+        grid.addWidget(label2, 0, 1)
+        grid.addWidget(label3, 1, 0)
+        grid.addWidget(label4, 1, 1)
+        grid.addWidget(label5, 1, 2)
+
+        central_widget.setLayout(grid)   # vbox, hbox, grid
 
 
 def main():
@@ -30,4 +47,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
