cc17c7efde2db8dbb79d63b654457a90219ce09a
SKYGOD <242455301+skyg6384-jpg@users.noreply.github.com>
2026-09-26 22:54:06 +0530

    70. PyQt5 buttons

--- main.py
+++ main.py
@@ -1,43 +1,28 @@
-# PyQt5 layouts
+# PyQt5 buttons
 import sys
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel,
-                             QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(700, 300, 500, 500)
+        self.button = QPushButton("click me!", self)
+        self.label = QLabel("Hello", self)
         self.initUI()
 
     def initUI(self):
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+        self.button = QPushButton("Click me!", self)
+        self.button.setGeometry(150, 200, 200, 100)
+        self.button.setStyleSheet("font-size: 30px;")
+        self.button.clicked.connect(self.on_click)
 
-        label1 = QLabel("#1", self)
-        label2 = QLabel("#2", self)
-        label3 = QLabel("#3", self)
-        label4 = QLabel("#4", self)
-        label5 = QLabel("#5", self)
+        self.label.setGeometry(150, 300, 200, 100)
+        self.label.setStyleSheet("font-size: 50px;")
 
-        label1.setStyleSheet("background-color: red;")
-        label2.setStyleSheet("background-color: yellow;")
-        label3.setStyleSheet("background-color: green;")
-        label4.setStyleSheet("background-color: blue;")
-        label5.setStyleSheet("background-color: purple;")
-
-        #vbox = QVBoxLayout()
-        #hbox = QHBoxLayout()
-        grid = QGridLayout()
-
-        grid.addWidget(label1, 0, 0)   # vbox, hbox, grid
-        grid.addWidget(label2, 0, 1)
-        grid.addWidget(label3, 1, 0)
-        grid.addWidget(label4, 1, 1)
-        grid.addWidget(label5, 1, 2)
-
-        central_widget.setLayout(grid)   # vbox, hbox, grid
-
+    def on_click(self):
+        self.label.setText("Goodbye")
+        #self.button.setDisabled(True)  # For Disabled
 
 def main():
     app = QApplication(sys.argv)
